4953a6b8c624bfe729b316be081a604c17686a8f
Anderson Dantas de Carvalho <andersondantas81@gmail.com>
2022-01-17 08:32:27 -0300

    Update Exercicio01.java
    Utilizando stream para ordenar a lista.

--- 65936000320/src/Graduado/Exercicio01.java
+++ 65936000320/src/Graduado/Exercicio01.java
@@ -13,22 +13,26 @@ public class Exercicio01 {
 		int cont = 1;
 		
 		do {
-			System.out.println("Informe o "+cont+"º valor:");
+			System.out.println("Informe o "+cont+"Âº valor:");
 			int valor = scanner.nextInt();
 			if(!lista.contains(valor)) {
 				lista.add(valor);
 			}else --cont;
 			cont++;
 		}while(cont <= 6);
+		scanner.close();
 		
-		System.out.println("O maior valor é: "+Collections.max(lista)+
+		System.out.println("O maior valor Ã©: "+Collections.max(lista)+
 				" no index "+lista.indexOf(Collections.max(lista)));
 		
-		System.out.println("O menor valor é: "+Collections.min(lista)+
+		System.out.println("O menor valor Ã©: "+Collections.min(lista)+
 				" no index "+lista.indexOf(Collections.min(lista)));
 		
-		Collections.sort(lista);
-		System.out.println(lista);
+		lista.stream()
+        	.sorted((p1, p2) -> p1.compareTo(p2))
+        	.forEach(p -> System.out.println(p));
+		//Collections.sort(lista);
+		//System.out.println(lista);
 		
 	}
 
